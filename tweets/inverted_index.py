0d7761d6f110ff15e81187d817ff00a1660f2faf
Kantha Girish G <kg2605@g.rit.edu>
2017-04-08 11:44:20 -0400

    Commented and organized the code for checkpoint-3 release.

--- tweets/inverted_index.py
+++ tweets/inverted_index.py
@@ -1,8 +1,13 @@
-__author__ = "Kantha Girish"
+__author__ = "Kantha Girish", "Pankaj Uchil Vasant", "Samana Katti"
 
 
 class InvertedIndex:
     """
+    This class implements the inverted index logic to create inverted index on trending
+    topics and tweets. In this case, the documents are the trends themselves and the
+    termList will be Twitter handles(Twitter usernames). An entry of python-twitter's
+    `Status` object, which is the Tweet, is added to the postings list against each trend
+    for each user, if he has tweeted under the trend.
     """
     __slots__ = ["twitterHandles", "trends", "indexLists"]
 
@@ -15,8 +20,11 @@ class InvertedIndex:
 
     def _populate(self, tweets):
         """
-        :param tweets:
-        :return:
+        :param tweets: A python dictionary containing trends as keys and list of tweets as
+        values against each trend.
+        :return: None
+
+        This is a private method used by the constructor to populate the inverted index object
         """
         for trendName in tweets:
             self.trends.append(trendName)
@@ -31,8 +39,11 @@ class InvertedIndex:
 
     def search(self, handle):
         """
-        :param handle:
-        :return:
+        :param handle: the twitter handle  for which the search is to be performed
+        :return: the list of tweets for the given twitter handle if exist, else an empty list.
+
+        This method returns the list of tweets for the given twitter handle if the handle exits
+        in the list of known twitter handles.
         """
         if handle in self.twitterHandles:
             return self.indexLists[self.twitterHandles.index(handle)]
@@ -41,8 +52,17 @@ class InvertedIndex:
 
     def getRelevantTweets(self, targetHandles):
         """
-        :param targetHandles:
-        :return:
+        :param targetHandles: A python dictionary of target twitter handles of popular
+        people/celebs with the keys as twitter handles.
+        :return: A python dictionary of keys as the trends and the values as list of tweets
+        filtered by the list of target twitter handles.
+                tweets = {
+                    "trend1": [tweet1, tweet2, ...],
+                    "trend2": [tweet3, tweet4, ...]
+                }
+
+        This method takes a dictionary of target twitter handles and returns the tweets of
+        those handles segregated into trends as a python dictionary.
         """
         tweets = {}
         for trend in self.trends:
@@ -56,7 +76,7 @@ class InvertedIndex:
 
     def __str__(self):
         """
-        :return:
+        :return: String representation of the InvertedIndex
         """
         result = ""
         for idx in range(len(self.twitterHandles)):
